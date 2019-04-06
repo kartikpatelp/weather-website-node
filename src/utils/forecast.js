be659d68e045f72b6108170913d92def9379713a
kartikpatel6390 <kartikpatelp@gmail.com>
2019-04-06 08:27:42 +0530

    Add extra information on index page

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,6 +1,7 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
+    console.log(latitude, longitude);
     const url = 'https://api.darksky.net/forecast/f76ae91d9d7aeef98c4e6f369f0e159a/'+ latitude +','+longitude;
 
     request({ url, json: true}, (error, { body }) => {
@@ -9,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
         }else if (body.error) {
             callback("Unable to find location", undefined)
         }else {
-            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain.')    
+            callback(undefined, body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degress out. There is a ' + body.currently.precipProbability + '% chance of rain. Highest Temprature: '+body.daily.data[0].temperatureHigh+' Lowest Temprature: '+body.daily.data[0].temperatureLow)    
         }
     })
 }
